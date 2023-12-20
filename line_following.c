48e15f6daa5750f5bbc464ed4e3024f206fdf403
kubo <kubo314159265@gmail.com>
2023-12-20 21:48:02 +0900

    プログラム書き込み

--- line_following.c
+++ line_following.c
@@ -4,13 +4,36 @@
 #include "D_EIO.h"
 
 void user_main(void) {
-    int current_front_sensor_values[3] = {};
-    int current_back_sensor_values[2] = {};
+    int current_sensor_values[9];
 
-    while(TRUE) {
-        for (int i = 0; i < 3; i++) {
-            current_front_sensor_values[i]
+    int plus = 90;
+    int minus = 100;
+    int straight = 75;
+    
+    int shikii = 300;
+
+    while (TRUE) {
+        //配列にすべて代入してループの中でセンサーの値が変わるのを防ぐ
+        //gAD[CN2] と gAD[1]は同じ
+        for (int i = 4; i < 9; i++) {
+            current_sensor_values[i] = gAD[i];
         }
-        
+
+        //前のセンサーで判定
+        if (current_sensor_values[CN7] >= shikii && current_sensor_values[CN9] <= shikii) {
+            motor(minus, plus);
+        } else if (current_sensor_values[CN7] <= shikii && current_sensor_values[CN9] >= shikii) {
+            motor(plus, minus);
+        } else {
+            motor(straight, straight);
+        }
+
+        //後ろのセンサーで判定
+        //後ろのセンサーは補助なのでまっすぐ進む処理は入れない
+        if (current_sensor_values[CN5] >= shikii && current_sensor_values[CN6] <= shikii) {
+            motor(minus, plus);
+        } else if (current_sensor_values[CN5] <= shikii && current_sensor_values[CN6] >= shikii) {
+            motor(plus, minus);
+        }
     }
 }
