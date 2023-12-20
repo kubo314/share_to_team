40af557a88f3823478118dc6209e0ac72593097f
kubo <kubo314159265@gmail.com>
2023-12-20 23:08:06 +0900

    T字路, Uターン追加

--- line_following.c
+++ line_following.c
@@ -7,7 +7,7 @@ void user_main(void) {
     int current_sensor_values[9];
 
     int plus = 90;
-    int minus = 100;
+    int minus = -100;
     int straight = 75;
     
     int shikii = 300;
@@ -15,8 +15,26 @@ void user_main(void) {
     while (TRUE) {
         //配列にすべて代入してループの中でセンサーの値が変わるのを防ぐ
         //gAD[CN2] と gAD[1]は同じ
-        for (int i = 4; i < 9; i++) {
+        for (int i = 2; i < 9; i++) {
             current_sensor_values[i] = gAD[i];
+        }
+        
+        //T字路
+        //行き
+        if (current_sensor_values[CN7] <= shikii || current_sensor_values[CN5] <= shikii) {
+            if (current_sensor_values[CN8] <= shikii) {
+                motor(minus, plus);
+                wait_ms(300);
+                continue;
+            }
+        }
+        //帰り
+        if (current_sensor_values[CN7] <= shikii || current_sensor_values[CN5] <= shikii) {
+            if (current_sensor_values[CN9] <= shikii || current_sensor_values[CN6] <= shikii) {
+                motor(plus, minus);
+                wait_ms(300);
+                continue;
+            }
         }
 
         //前のセンサーで判定
@@ -27,13 +45,25 @@ void user_main(void) {
         } else {
             motor(straight, straight);
         }
-
         //後ろのセンサーで判定
-        //後ろのセンサーは補助なのでまっすぐ進む処理は入れない
+        //後ろのセンサーは補助なので直進の処理は入れない
         if (current_sensor_values[CN5] >= shikii && current_sensor_values[CN6] <= shikii) {
             motor(minus, plus);
         } else if (current_sensor_values[CN5] <= shikii && current_sensor_values[CN6] >= shikii) {
             motor(plus, minus);
         }
+
+        //タワーでUターン
+        //CN3, CN4が他の値と干渉している?ためコメントアウト
+        /*
+        if (current_sensor_values[CN3] >= shikii || current_sensor_values[CN4] >= shikii) {
+            motor(0, 0);
+            wait_ms(100);
+            motor(-75, -75);
+            wait_ms(400);
+            motor(100, -100);
+            wait_ms(600);
+        }
+        */
     }
 }
