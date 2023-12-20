cf5703827e8b31d98d30f1d4a900daface2e396d
kubo <kubo314159265@gmail.com>
2023-12-20 21:10:15 +0900

    新規作成

---
+++ line_following.c
@@ -0,0 +1,16 @@
+#include "D_Main.h"
+#include "D_I2C.h"
+#include "D_SIO.h"
+#include "D_EIO.h"
+
+void user_main(void) {
+    int current_front_sensor_values[3] = {};
+    int current_back_sensor_values[2] = {};
+
+    while(TRUE) {
+        for (int i = 0; i < 3; i++) {
+            current_front_sensor_values[i]
+        }
+        
+    }
+}
